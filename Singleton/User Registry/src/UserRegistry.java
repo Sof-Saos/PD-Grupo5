cf92c9ff9a61d39f83b31814652b3a7ec4eae6be
ad-nicolas <duquenicolas391@gmail.com>
2023-09-01 10:34:01 -0500

    Primer commit del ejercicio singleton

--- Singleton/User Registry/src/UserRegistry.java
+++ Singleton/User Registry/src/UserRegistry.java
@@ -1,2 +1,34 @@
+import java.util.ArrayList;
+
 public class UserRegistry {
+    private static UserRegistry instance;
+    private ArrayList<User> userList;
+
+    private UserRegistry() {
+        userList = new ArrayList<>();
+    }
+
+    public static synchronized UserRegistry getInstance() {
+        if (instance == null) {
+            instance = new UserRegistry();
+        }
+        return instance;
+    }
+
+    public void addUser(User user) {
+        userList.add(user);
+    }
+
+    public List<User> getUsers() {
+        return userList;
+    }
+
+    public User getUserByUsername(String username) {
+        for (User user : userList) {
+            if (user.getUsername().equals(username)) {
+                return user;
+            }
+        }
+        return null;
+    }
 }
